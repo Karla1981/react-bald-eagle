15509c91f6a9fc8539d43f32e301e9dcb5a01892
Alona Vladymyrova <alona@Alonas-MacBook-Pro.local>
2023-02-26 17:34:25 -0800

    Changes to fix problems in lesson 1.3. implemented.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App tab="home" />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+//---------------
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>,
-//   document.getElementById('root')
+//   document.getElementById("root")
+// );
+//---------------
+
+// import ReactDOM from "react-dom";
+// import React from "react";
+// //import { createRoot } from "react-dom/client";
+// import App from "./App";
+
+// // const container = document.getElementById("root");
+// // const root = createRoot(container);
+// // root.render(<App tab="home" />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
 // );
